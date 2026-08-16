b75e7a1f84d2fe940308766ee3edaa8292c6a867
Ian Seyler <ian.seyler@returninfinity.com>
2026-08-16 11:38:07 -0400

    Update 2-run.sh

--- 2-run.sh
+++ 2-run.sh
@@ -15,6 +15,16 @@ if ! ip link show tap0 > /dev/null 2>&1; then
 	echo -e "${BOLD}Warning${NORMAL}: tap device 'tap0' not found. VM will be started without network access. Run 'BareMetal-Firecracker/scripts/mkbr0.sh' to enable it." >&2
 fi
 
+# Start the VM via Firecracker
 ./baremetal.sh start
-sleep 15
+
+# Check for 'screen`. If found attach to it's session - Otherwise sleep for 15 seconds
+if ! command -v screen > /dev/null 2>&1; then
+	sleep 15
+else
+	./baremetal.sh attach
+fi
+
+# Display the full VM Output
+echo -e "${BOLD}VM Output${NORMAL}:"
 ./baremetal.sh output --full
